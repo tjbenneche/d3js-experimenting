eb121b5006afc8284b7f2b04f1cfe12c6c17ad14
Tom Benneche <tom@tjbenneche.com>
2014-12-18 15:44:27 -0600

    pushing up

--- coffee/analytics.coffee
+++ coffee/analytics.coffee
@@ -46,6 +46,23 @@ xAxis = d3.svg.axis()
   .scale(x)
   .orient("bottom")
 
+updateAxes = () ->
+  chart_svg.selectAll('.axis')
+    .remove()
+  chart_svg.append('g')
+    .attr('class', 'x axis')
+    .attr("transform", "translate(0," + chart_height + ")")
+    .call(xAxis);
+  chart_svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Sessions");
+
 yAxis = d3.svg.axis()
   .scale(y)
   .orient("left")
@@ -53,12 +70,6 @@ yAxis = d3.svg.axis()
 area = d3.svg.area().x((d) ->
   x d.time
 ).y0(chart_height).y1((d) ->
-  y d.value
-)
-
-line = d3.svg.line().x((d) ->
-  x d.time
-).y((d) ->
   y d.value
 )
 
@@ -72,106 +83,97 @@ chart_svg = d3.select('body')
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
+populateData = (url) ->
+  #Load json file with today's data
+  d3.json url, (error, data) ->
 
-#Load json file with today's data
-d3.json "twentyfourhours.json", (error, data) ->
-  moduleArray = data.app_analytic.session_count_percentages
-  i = 0
-  percentages = []
-  names = []
-  while i < moduleArray.length
-    percentages.push(moduleArray[i][1])
-    names.push(moduleArray[i].module_name)
-    i++
-  arcs = pie_svg.selectAll('g.arc')
-    .data(pie(percentages))
-    .enter()
-    .append('g')
-    .attr('class', 'arc')
-    .attr('transform', 'translate(' + pie_outerRadius + ',' + pie_outerRadius + ')')
+    objectName = Object.keys(data)[0]
 
-  arcs.append('path')
-    .attr('fill', (d, i) ->
-      color(i)
+    moduleArray = data[objectName].session_count_percentages
+    i = 0
+    percentages = []
+    names = []
+    while i < moduleArray.length
+      percentages.push(moduleArray[i][1])
+      names.push(moduleArray[i].module_name)
+      i++
+    arcs = pie_svg.selectAll('g.arc')
+      .data(pie(percentages))
+      .enter()
+      .append('g')
+      .attr('class', 'arc')
+      .attr('transform', 'translate(' + pie_outerRadius + ',' + pie_outerRadius + ')')
+
+    arcs.append('path')
+      .attr('fill', (d, i) ->
+        color(i)
+      )
+      .attr('d', pie_arc)
+
+    arcs.append('text')
+      .attr('transform', (d) ->
+        'translate(' + pie_arc.centroid(d) + ')'
+      )
+      .attr('text-anchor', 'middle')
+      .text( (d) ->
+        d.value + '%'
+      )
+      .style({
+        fill: 'white'
+        })
+
+
+    # Generate three boxes for session count, avg length, and retention
+
+    overall_session_data = [data[objectName].session_count, data[objectName].average_session_length, data[objectName].retention_rate]
+
+    # need to figure out how to conditionally add units
+    session_boxes = d3.select('.session-data-section')
+      .selectAll('.session-box')
+      .data(overall_session_data)
+      .text( (d) ->
+        d
+      )
+
+    # Generate area chart for overall app data
+    overall_app_chart_data = data[objectName].session_count_graph_data
+
+    overall_app_chart_data.forEach( (d) ->
+      d.time = d[0]
+      d.value = d[1]
     )
-    .attr('d', pie_arc)
 
-  arcs.append('text')
-    .attr('transform', (d) ->
-      'translate(' + pie_arc.centroid(d) + ')'
-    )
-    .attr('text-anchor', 'middle')
-    .text( (d) ->
-      d.value + '%'
-    )
-    .style({
-      fill: 'white'
-      })
+    x.domain(overall_app_chart_data.map( (d) ->
+        d.time
+    ))
+
+    y.domain([0, d3.max(overall_app_chart_data, (d) ->
+        d.value
+    )])
 
 
-  # Generate three boxes for session count, avg length, and retention
+    unless $('.area').length > 0
+      chart_svg.append('path')
+        .attr('class', 'area')
 
-  overall_session_data = [data.app_analytic.session_count, data.app_analytic.average_session_length, data.app_analytic.retention_rate]
-
-  # need to figure out how to conditionally add units
-  session_boxes = d3.select('.session-data-section')
-    .selectAll('.session-box')
-    .data(overall_session_data)
-    .text( (d) ->
-      d
-    )
-
-
-
-
-  # Generate area chart for overall app data
-  overall_app_chart_data = data.app_analytic.session_count_graph_data
-
-  overall_app_chart_data.forEach( (d) ->
-    d.time = d[0]
-    d.value = d[1]
-  )
-
-  x.domain(overall_app_chart_data.map( (d) ->
-      d.time
-  ))
-
-  y.domain([0, d3.max(overall_app_chart_data, (d) ->
-      d.value
-  )])
-
-  console.log(x.domain())
-  console.log(y.domain())
-
-  chart_svg.append('path')
-    .datum(overall_app_chart_data)
-    .attr('class', 'area')
-    .attr('d', area)
-
-  chart_svg.append("path")
+    chart_svg.select('.area')
       .datum(overall_app_chart_data)
-      .attr("class", "line")
-      .attr("d", line)
+      .attr('class', 'area ' + objectName)
+      .transition()
+        .duration(1000)
+        .attr('d', area)
 
-  chart_svg.append('g')
-    .attr('class', 'x axis')
-    .attr("transform", "translate(0," + chart_height + ")")
-    .call(xAxis);
+    updateAxes()
 
-  chart_svg.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Sessions");
+$ ->
+  populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/24hour")
 
-  chart_svg.selectAll(".dot").data(overall_app_chart_data.filter((d) ->
-      d.value
-    )).enter().append("circle")
-    .attr("class", "dot")
-    .attr("cx", line.x())
-    .attr("cy", line.y())
-    .attr "r", 3.5
+
+$(document).on 'click', '#weekly', (e) ->
+  populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/7day")
+
+$(document).on 'click', '#daily', (e) ->
+  populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/24hour")
+
+$(document).on 'click', '#monthly', (e) ->
+  populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/30day")
--- js/analytics.js
+++ js/analytics.js
@@ -1,5 +1,5 @@
 (function() {
-  var area, chart_height, chart_svg, chart_width, color, line, margin, pie, pie_arc, pie_height, pie_innerRadius, pie_outerRadius, pie_svg, pie_width, x, xAxis, y, yAxis;
+  var area, chart_height, chart_svg, chart_width, color, margin, pie, pie_arc, pie_height, pie_innerRadius, pie_outerRadius, pie_svg, pie_width, populateData, updateAxes, x, xAxis, y, yAxis;
 
   margin = {
     top: 50,
@@ -34,6 +34,12 @@
 
   xAxis = d3.svg.axis().scale(x).orient("bottom");
 
+  updateAxes = function() {
+    chart_svg.selectAll('.axis').remove();
+    chart_svg.append('g').attr('class', 'x axis').attr("transform", "translate(0," + chart_height + ")").call(xAxis);
+    return chart_svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Sessions");
+  };
+
   yAxis = d3.svg.axis().scale(y).orient("left");
 
   area = d3.svg.area().x(function(d) {
@@ -42,62 +48,71 @@
     return y(d.value);
   });
 
-  line = d3.svg.line().x(function(d) {
-    return x(d.time);
-  }).y(function(d) {
-    return y(d.value);
-  });
-
   chart_svg = d3.select('body').append('svg').attr("width", chart_width + margin.left + margin.right).attr("height", chart_height + margin.top + margin.bottom).attr('class', 'areachart').append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  d3.json("twentyfourhours.json", function(error, data) {
-    var arcs, i, moduleArray, names, overall_app_chart_data, overall_session_data, percentages, session_boxes;
-    moduleArray = data.app_analytic.session_count_percentages;
-    i = 0;
-    percentages = [];
-    names = [];
-    while (i < moduleArray.length) {
-      percentages.push(moduleArray[i][1]);
-      names.push(moduleArray[i].module_name);
-      i++;
-    }
-    arcs = pie_svg.selectAll('g.arc').data(pie(percentages)).enter().append('g').attr('class', 'arc').attr('transform', 'translate(' + pie_outerRadius + ',' + pie_outerRadius + ')');
-    arcs.append('path').attr('fill', function(d, i) {
-      return color(i);
-    }).attr('d', pie_arc);
-    arcs.append('text').attr('transform', function(d) {
-      return 'translate(' + pie_arc.centroid(d) + ')';
-    }).attr('text-anchor', 'middle').text(function(d) {
-      return d.value + '%';
-    }).style({
-      fill: 'white'
+  populateData = function(url) {
+    return d3.json(url, function(error, data) {
+      var arcs, i, moduleArray, names, objectName, overall_app_chart_data, overall_session_data, percentages, session_boxes;
+      objectName = Object.keys(data)[0];
+      moduleArray = data[objectName].session_count_percentages;
+      i = 0;
+      percentages = [];
+      names = [];
+      while (i < moduleArray.length) {
+        percentages.push(moduleArray[i][1]);
+        names.push(moduleArray[i].module_name);
+        i++;
+      }
+      arcs = pie_svg.selectAll('g.arc').data(pie(percentages)).enter().append('g').attr('class', 'arc').attr('transform', 'translate(' + pie_outerRadius + ',' + pie_outerRadius + ')');
+      arcs.append('path').attr('fill', function(d, i) {
+        return color(i);
+      }).attr('d', pie_arc);
+      arcs.append('text').attr('transform', function(d) {
+        return 'translate(' + pie_arc.centroid(d) + ')';
+      }).attr('text-anchor', 'middle').text(function(d) {
+        return d.value + '%';
+      }).style({
+        fill: 'white'
+      });
+      overall_session_data = [data[objectName].session_count, data[objectName].average_session_length, data[objectName].retention_rate];
+      session_boxes = d3.select('.session-data-section').selectAll('.session-box').data(overall_session_data).text(function(d) {
+        return d;
+      });
+      overall_app_chart_data = data[objectName].session_count_graph_data;
+      overall_app_chart_data.forEach(function(d) {
+        d.time = d[0];
+        return d.value = d[1];
+      });
+      x.domain(overall_app_chart_data.map(function(d) {
+        return d.time;
+      }));
+      y.domain([
+        0, d3.max(overall_app_chart_data, function(d) {
+          return d.value;
+        })
+      ]);
+      if (!($('.area').length > 0)) {
+        chart_svg.append('path').attr('class', 'area');
+      }
+      chart_svg.select('.area').datum(overall_app_chart_data).attr('class', 'area ' + objectName).transition().duration(1000).attr('d', area);
+      return updateAxes();
     });
-    overall_session_data = [data.app_analytic.session_count, data.app_analytic.average_session_length, data.app_analytic.retention_rate];
-    session_boxes = d3.select('.session-data-section').selectAll('.session-box').data(overall_session_data).text(function(d) {
-      return d;
-    });
-    overall_app_chart_data = data.app_analytic.session_count_graph_data;
-    overall_app_chart_data.forEach(function(d) {
-      d.time = d[0];
-      return d.value = d[1];
-    });
-    x.domain(overall_app_chart_data.map(function(d) {
-      return d.time;
-    }));
-    y.domain([
-      0, d3.max(overall_app_chart_data, function(d) {
-        return d.value;
-      })
-    ]);
-    console.log(x.domain());
-    console.log(y.domain());
-    chart_svg.append('path').datum(overall_app_chart_data).attr('class', 'area').attr('d', area);
-    chart_svg.append("path").datum(overall_app_chart_data).attr("class", "line").attr("d", line);
-    chart_svg.append('g').attr('class', 'x axis').attr("transform", "translate(0," + chart_height + ")").call(xAxis);
-    chart_svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Sessions");
-    return chart_svg.selectAll(".dot").data(overall_app_chart_data.filter(function(d) {
-      return d.value;
-    })).enter().append("circle").attr("class", "dot").attr("cx", line.x()).attr("cy", line.y()).attr("r", 3.5);
+  };
+
+  $(function() {
+    return populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/24hour");
+  });
+
+  $(document).on('click', '#weekly', function(e) {
+    return populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/7day");
+  });
+
+  $(document).on('click', '#daily', function(e) {
+    return populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/24hour");
+  });
+
+  $(document).on('click', '#monthly', function(e) {
+    return populateData("http://localhost:3000/api/v1/apps/2mNFJaz4/analytics/30day");
   });
 
 }).call(this);
